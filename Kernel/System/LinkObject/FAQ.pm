c2816632d1474bc20d5228cada7cb5018095288f
Udo Bretz <udo.bretz@otrs.com>
2013-03-25 18:25:02 +0000

    Improved permission checks in LinkObject.

--- Kernel/System/LinkObject/FAQ.pm
+++ Kernel/System/LinkObject/FAQ.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/System/LinkObject/FAQ.pm - to link faq objects
-# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2013 OTRS AG, http://otrs.org/
 # --
-# $Id: FAQ.pm,v 1.17 2012-01-26 16:37:39 mh Exp $
+# $Id: FAQ.pm,v 1.17.2.1 2013-03-25 18:25:02 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -14,10 +14,11 @@ package Kernel::System::LinkObject::FAQ;
 use strict;
 use warnings;
 
+use Kernel::System::Group;
 use Kernel::System::FAQ;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.17 $) [1];
+$VERSION = qw($Revision: 1.17.2.1 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -32,7 +33,8 @@ sub new {
     }
 
     # create additional objects
-    $Self->{FAQObject} = Kernel::System::FAQ->new( %{$Self} );
+    $Self->{GroupObject} = Kernel::System::Group->new( %{$Self} );
+    $Self->{FAQObject}   = Kernel::System::FAQ->new( %{$Self} );
 
     return $Self;
 }
@@ -71,12 +73,12 @@ sub LinkListWithData {
         return;
     }
 
-    for my $LinkType ( keys %{ $Param{LinkList} } ) {
+    for my $LinkType ( sort keys %{ $Param{LinkList} } ) {
 
-        for my $Direction ( keys %{ $Param{LinkList}->{$LinkType} } ) {
+        for my $Direction ( sort keys %{ $Param{LinkList}->{$LinkType} } ) {
 
             FAQID:
-            for my $FAQID ( keys %{ $Param{LinkList}->{$LinkType}->{$Direction} } ) {
+            for my $FAQID ( sort keys %{ $Param{LinkList}->{$LinkType}->{$Direction} } ) {
 
                 # get faq data
                 my %FAQData = $Self->{FAQObject}->FAQGet(
@@ -97,6 +99,77 @@ sub LinkListWithData {
     }
 
     return 1;
+}
+
+=item ObjectPermission()
+
+checks read permission for a given object and UserID.
+
+    $Permission = $LinkObject->ObjectPermission(
+        Object  => 'FAQ',
+        Key     => 123,
+        UserID  => 1,
+    );
+
+=cut
+
+sub ObjectPermission {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Object Key UserID)) {
+        if ( !$Param{$Argument} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    # check module registry of AgentFAQZoom
+    my $ModuleReg = $Self->{ConfigObject}->Get('Frontend::Module')->{AgentFAQZoom};
+
+    # do not grant access if frontend module is not registered
+    return if !$ModuleReg;
+
+    # grant access if module permisson has no Group or GroupRo defined
+    if ( !$ModuleReg->{GroupRo} && !$ModuleReg->{Group} ) {
+        return 1;
+    }
+
+    PERMISSION:
+    for my $Permission (qw(GroupRo Group)) {
+
+        next PERMISSION if !$ModuleReg->{$Permission};
+        next PERMISSION if ref $ModuleReg->{$Permission} ne 'ARRAY';
+
+        for my $Group ( @{ $ModuleReg->{$Permission} } ) {
+
+            # get the group id
+            my $GroupID = $Self->{GroupObject}->GroupLookup( Group => $Group );
+
+            my $Type;
+            if ( $Permission eq 'GroupRo' ) {
+                $Type = 'ro';
+            }
+            elsif ( $Permission eq 'Group' ) {
+                $Type = 'rw';
+            }
+
+            # get user groups, where the user has the appropriate privilege
+            my %Groups = $Self->{GroupObject}->GroupMemberList(
+                UserID => $Param{UserID},
+                Type   => $Type,
+                Result => 'HASH',
+            );
+
+            # grant access if agent is a member in the group
+            return 1 if $Groups{$GroupID};
+        }
+    }
+
+    return;
 }
 
 =item ObjectDescriptionGet()
