f0d194c9a05140cf93d263bab692c17526f3db88
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2014-07-31 21:03:23 -0500

    Fixed: FAQ history isn't sorted by created (bug#10452).
    Conflicts:
    	CHANGES-FAQ

--- Kernel/System/FAQ.pm
+++ Kernel/System/FAQ.pm
@@ -1391,7 +1391,11 @@ sub FAQHistoryGet {
     }
 
     return if !$Self->{DBObject}->Prepare(
-        SQL  => 'SELECT name, created, created_by FROM faq_history WHERE item_id = ?',
+        SQL => '
+            SELECT name, created, created_by
+            FROM faq_history
+            WHERE item_id = ?
+            ORDER BY created, id',
         Bind => [ \$Param{ItemID} ],
     );
 
