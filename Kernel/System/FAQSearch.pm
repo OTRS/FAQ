20596e2d4e96fbc34f131c3cecad0c43b5eccb9b
Martin Gruner <martin.gruner@otrs.com>
2016-06-28 08:59:05 +0200

    Fixed Perl warning.

--- Kernel/System/FAQSearch.pm
+++ Kernel/System/FAQSearch.pm
@@ -478,7 +478,7 @@ sub FAQSearch {
 
         my @States = map { $DBObject->Quote( $_, 'Integer' ) } keys %{ $Param{States} };
 
-        return if scalar @States != keys $Param{States};
+        return if scalar @States != keys %{ $Param{States} };
 
         my $InString = $Self->_InConditionGet(
             TableColumn => 's.type_id',
